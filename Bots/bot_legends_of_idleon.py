fdc8050b924c698a96653dddcd5475c2909a1384
Thiago Brazileiro <brazileiro.thiago@gmail.com>
2021-04-17 14:11:50 -0300

    wip

--- Bots/bot_legends_of_idleon.py
+++ Bots/bot_legends_of_idleon.py
@@ -1,3 +1,15 @@
 import pyautogui, time
+from python_imagesearch.imagesearch import imagesearch
 
-# WIP
+pos1 = imagesearch("./imgs/chop.png")
+while True:
+    if pos1[0] == -1:
+        print('Chop not found')
+        pos1 = imagesearch("./imgs/chop.png")
+    else:
+        pos = imagesearch("./imgs/mini_game_cutting.png")
+        if pos[0] != -1:
+            print("position : ", pos[0], pos[1])
+            pyautogui.click(pos1)
+        else:
+            print("image not found")
