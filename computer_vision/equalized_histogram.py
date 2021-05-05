4023a4e1990ee53ad3e8d9401936d93f8014f8bc
Thiago Brazileiro <brazileiro.thiago@gmail.com>
2021-05-05 18:08:16 -0300

    add colored histogram too

--- computer_vision/equalized_histogram.py
+++ computer_vision/equalized_histogram.py
@@ -2,15 +2,36 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread(r'imgs\mini_game_cutting.png', 0)
-equalized_image = cv2.equalizeHist(img)
+def equalize_without_color():
+    img = cv2.imread(r'imgs\mini_game_cutting.png', 0)
+    equalized_image = cv2.equalizeHist(img)
 
-fig = plt.figure(figsize=(20,5))
+    fig = plt.figure(figsize=(20,5))
 
-ax1 = fig.add_subplot(121)
-plt.imshow(img, cmap=plt.cm.gray)
+    ax1 = fig.add_subplot(121)
+    plt.imshow(img, cmap=plt.cm.gray)
 
-ax2 = fig.add_subplot(122)
-plt.imshow(equalized_image, cmap=plt.cm.gray)
+    ax2 = fig.add_subplot(122)
+    plt.imshow(equalized_image, cmap=plt.cm.gray)
 
-plt.show()
+
+def equalize_with_color():
+    img = cv2.imread(r'imgs\mini_game_cutting.png', 1)
+
+    blue, green, red = cv2.split(img)
+
+    equalized_blue = cv2.equalizeHist(blue)
+    equalized_green = cv2.equalizeHist(green)
+    equalized_red = cv2.equalizeHist(red)
+
+    fig = plt.figure(figsize=(20,5))
+
+    ax1 = fig.add_subplot(121)
+    plt.hist(green.ravel(), 256, [0,256])
+
+    ax2 = fig.add_subplot(122)
+    plt.hist(equalized_green.ravel(), 256, [0,256])
+
+    plt.show()
+
+equalize_with_color()
