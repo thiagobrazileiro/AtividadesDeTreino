04f3eaa7c942a2739bc4dec358f1ca0a369ace24
Thiago Brazileiro <brazileiro.thiago@gmail.com>
2021-05-18 18:47:59 -0300

    wip: tring to get a pixel color under leaf
    Dont works yet

--- computer_vision/object_detection_screen/screen_manipulate.py
+++ computer_vision/object_detection_screen/screen_manipulate.py
@@ -6,7 +6,7 @@ from time import time
 from windowcapture import WindowCapture
 
 
-img_folder = cv.imread('imgs/mini_game_cutting.png')
+img_folder = cv.imread('imgs/leaf.png')
 
 # initialize the WindowCapture class
 wincap = WindowCapture('Legends Of Idleon')
@@ -28,17 +28,22 @@ while True:
             first = False
             mouse.position = (x, y)
         
-        # needle_w = img_folder.shape[1]
-        # needle_h = img_folder.shape[0]
-        mouse.click(Button.left, 1)
-        # top_left = max_loc
-        # bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
+        needle_w = img_folder.shape[1]
+        needle_h = img_folder.shape[0]
+        # mouse.click(Button.left, 1)
+        top_left = max_loc
+        bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
+        bottom_left = (top_left[0], top_left[1] + needle_h)
 
-        # cv.rectangle(screenshot, top_left, bottom_right, 
-        #                 color=(0, 255, 0), thickness=2, lineType=cv.LINE_4)
+        color = screenshot[bottom_left]
+        
+        print(color)
+
+        cv.rectangle(screenshot, top_left, bottom_right, 
+                        color=(0, 255, 0), thickness=2, lineType=cv.LINE_4)
 
 
-    #cv.imshow('Computer Vision', screenshot)
+    cv.imshow('Computer Vision', screenshot)
 
     print('FPS {}'.format(1 / (time() - loop_time)))
     loop_time = time()
