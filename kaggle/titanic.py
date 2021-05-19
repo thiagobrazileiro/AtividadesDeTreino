9908775c348e94c0e613ef996f38495393edd3f7
Thiago Brazileiro <brazileiro.thiago@gmail.com>
2021-05-19 19:00:19 -0300

    wip: first steps

---
+++ kaggle/titanic.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+test = pd.read_csv('data/titanic/test.csv')
+gender_submission = pd.read_csv('data/titanic/gender_submission.csv')
+train = pd.read_csv('data/titanic/train.csv')
+print(train)
